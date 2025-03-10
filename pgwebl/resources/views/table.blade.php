45e8944cd77435dad90df698082d6a338e3091de
Aentyr <naufalhidayat241@gmail.com>
2025-03-11 03:54:59 +0700

    first commit

---
+++ pgwebl/resources/views/table.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.template')
+
+@section('content')
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Nim</th>
+            <th>Kelas</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>1</th>
+            <th>Imsandry</th>
+            <th>518883</th>
+            <th>SIG B</th>
+        </tr>
+        <tr>
+            <th>2</th>
+            <th>Naufal</th>
+            <th>520500</th>
+            <th>SIG B</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>Sadewa</th>
+            <th>523802</th>
+            <th>SIG B</th>
+        </tr>
+    </tbody>
+</table>
+@endsection
---
+++ pgwebl/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\PointsController;
+use App\Http\Controllers\TableController;
+use App\Models\PointsModel;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PointsController::class, 'index']) -> name ('map');
+
+Route::get('table', [TableController::class, 'index']) -> name ('table');
+
+Route::resource('points', PointsController::class);
